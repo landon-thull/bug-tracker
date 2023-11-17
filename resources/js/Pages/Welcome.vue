f718d83c24d3aba42530758f650825835e157fab
Landon Thull <landonthull.tx@gmail.com>
2023-11-16 20:25:13 -0800

    style: update welcome page colors

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,29 +13,30 @@ defineProps<{
     <Head title="Welcome" />
 
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 selection:bg-red-500 selection:text-white"
     >
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="flex justify-center flex-col">
-                <h1 class="text-3xl font-bold text-white">Bug Tracker</h1>
-                <div v-if="canLogin" class="flex justify-center mt-4">
+            <div class="flex justify-center align-middle flex-col bg-white p-8 rounded shadow">
+                <h1 class="text-3xl text-center font-bold text-gray-800">Bug Tracker</h1>
+                <div class=" h-0.5 rounded-full my-4 bg-gray-500 w-64"></div>
+                <div v-if="canLogin" class="flex w-full justify-around justify-center">
                     <Link
                         v-if="$page.props.auth.user"
                         :href="route('dashboard')"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                        class="font-semibold text-gray-600 hover:text-gray-900"
                     >
                         Dashboard
                     </Link>
                     <template v-else>
                         <Link
                             :href="route('login')"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                            class="font-semibold text-gray-600 hover:text-gray-900"
                         >Log in</Link>
 
                         <Link
                             v-if="canRegister"
                             :href="route('register')"
-                            class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+                            class="ms-4 font-semibold text-gray-600 hover:text-gray-900"
                         >Register</Link
                         >
                     </template>
